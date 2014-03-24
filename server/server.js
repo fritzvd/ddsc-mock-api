42b9f0df75938cee88f159b75880ff6ba42e0318
fritzvd <f.vandeventer@gmail.com>
2014-03-24 14:18:01 +0100

    added client

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.configure(function () {
 	// app.use(express.static(__dirname + '../client'));
 	app.all('*', function(req, res, next) {
 	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header('Access-Control-Allow-Credentials', false);
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	  next();
